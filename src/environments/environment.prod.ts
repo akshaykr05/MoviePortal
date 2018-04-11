f798cad5cbd1c287f7b9487dda4614b2408ef1c8
akshaykr05 <vishwas@propertyguru.com.sg>
2018-04-11 23:35:51 +0530

    navigation bar added and firebase configured.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyB5syIPXh1RV42voXG5gA2sXyWWdr0GMX4",
+    authDomain: "movieportal-012.firebaseapp.com",
+    databaseURL: "https://movieportal-012.firebaseio.com",
+    projectId: "movieportal-012",
+    storageBucket: "",
+    messagingSenderId: "316422887207"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyB5syIPXh1RV42voXG5gA2sXyWWdr0GMX4",
+    authDomain: "movieportal-012.firebaseapp.com",
+    databaseURL: "https://movieportal-012.firebaseio.com",
+    projectId: "movieportal-012",
+    storageBucket: "",
+    messagingSenderId: "316422887207"
+  }
 };
